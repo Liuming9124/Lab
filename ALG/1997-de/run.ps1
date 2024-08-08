aa03ac8ca7fe6eddd342f154df8f7dbdd0479b7c
Liuming9124 <a723479.a12@gmail.com>
2024-08-08 15:09:30 +0800

    Run Powershell Script

---
+++ ALG/1997-de/run.ps1
@@ -0,0 +1,13 @@
+# 1. 詢問執行檔名稱
+$executable = Read-Host "add executable name"
+# 2. 詢問執行次數
+$executionCount = Read-Host "add execution count"
+# 3. 讀取 run.txt 檔案
+$filePath = "run.txt"
+$commands = Get-Content -Path $filePath
+
+foreach ($command in $commands) {
+    $fullCommand = "$executable $executionCount $command"
+    Write-Host "running: $fullCommand"
+    Invoke-Expression $fullCommand
+}
