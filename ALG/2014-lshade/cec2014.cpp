68c71ebda0ed58f3037973918f8a6888a9140855
Liuming9124 <a723479.a12@gmail.com>
2024-08-20 02:54:03 +0800

    0819 temp

---
+++ ALG/2014-lshade/cec2014.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <memory>
+#include "../../Reference/cec14-c-code/cec14_test_func.cpp"
+#include "../Tool.h"
+using namespace std;
+
+
+#include <WINDOWS.H>    
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+
+
+void cec14_test_func(double *, double *,int,int,int);
+
+double *OShift,*M,*y,*z,*x_bound;
+int ini_flag=0,n_flag,func_flag,*SS;
+
+
+
+class Problem
+{
+public:
+    void setStrategy(int func) {
+        this->strategy = func;
+    }
+    
+    double executeStrategy(const vector<double>& xx, int dim) const {
+        double f;
+        cec14_test_func( const_cast<double*>(&xx[0]), &f, dim, 1, strategy);
+        return f;
+    }
+
+    double getBounderMin() const {
+        return -100.0;
+    }
+
+    double getBounderMax() const {
+        return 100.0;
+    }
+
+private:
+    int strategy;
+
+};
--- ALG/test.cpp
+++ ALG/test.cpp
@@ -1,10 +1,27 @@
 #include "Tool.h"
+#include <vector>
 #include <iostream>
+using namespace std;
+
+void asyfunc (double *x)
+{
+	int i;
+    for (i=0; i<10; i++)
+    {
+        cout << x[i] << endl;
+    }
+}
 
 int main(){
 
-    Tool test;
-    for (int i = 0; i < 10; i++){
-        cout << test.rand_int(0, 10) << endl;
+    // Tool test;
+    // for (int i = 0; i < 10; i++){
+    //     cout << test.rand_int(0, 10) << endl;
+    // }
+    vector<double> x;
+    x.resize(10);
+    for (int i = 0; i < x.size(); i++){
+        x[i] = i;
     }
+    asyfunc(&x[0]);
 }
